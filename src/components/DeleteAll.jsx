12e240fafa0f852af359cec41803057b250e0e64
Hridya Sharma <hridyasharma2529@gmail.com>
2026-06-15 19:06:42 +0530

    initial commit

---
+++ src/components/DeleteAll.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import { Button } from "@material-tailwind/react";
+
+function DeleteAll() {
+    return <Button color="cyan">Delete All</Button>
+}
+
+
+export default DeleteAll;
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import TodoForm from "./TodoForm";
+import TodoItem from "./TodoItem";
+import DeleteAll from "./DeleteAll";
+
+export { TodoForm, TodoItem, DeleteAll }
